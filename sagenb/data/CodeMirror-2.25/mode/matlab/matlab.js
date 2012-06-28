6a3a59ac19e69fd6f58f1068eb5ebf3188dd07ec
Jorge Ruiz <jorgerev90@gmail.com>
2012-06-27 19:13:20 -0500

    Keywords for matlab mode added.

--- sagenb/data/CodeMirror-2.25/mode/matlab/matlab.js
+++ sagenb/data/CodeMirror-2.25/mode/matlab/matlab.js
@@ -1,5 +1,20 @@
 CodeMirror.defineMode("matlab", function() {
 
+  var keywords = function(){
+    function kw(type) {return {type: type, style: "keyword"};}
+    var A = kw("keyword a"), B = kw("keyword b"), C = kw("keyword c");
+    var operator = kw("operator"), atom = {type: "atom", style: "atom"};
+    return {
+      "if": A, "while": A, "with": A, "else": B, "do": B, "try": B, "finally": B,
+      "return": C, "break": C, "continue": C, "new": C, "delete": C, "throw": C,
+      "var": kw("var"), "const": kw("var"), "let": kw("var"),
+      "function": kw("function"), "catch": kw("catch"),
+      "for": kw("for"), "switch": kw("switch"), "case": kw("case"), "default": kw("default"),
+      "in": operator, "typeof": operator, "instanceof": operator,
+      "true": atom, "false": atom, "null": atom, "undefined": atom, "NaN": atom, "Infinity": atom
+    };
+  }();
+
   var TOKEN_NAMES = {
     '+': 'tag',
     '-': 'string',
